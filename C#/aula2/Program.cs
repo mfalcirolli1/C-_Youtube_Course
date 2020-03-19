fc0425c37250374bfc5427db14c0dd7b25171cd3
Matheus Falcirolli <fmatheus8@gmail.com>
2020-03-18 23:38:13 -0300

    Lesson Two

---
+++ C#/aula2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo!");
+            if(args.GetLength(0)>0){
+                Console.Write(args.GetValue(0));
+            }
+        }
+    }
+}
